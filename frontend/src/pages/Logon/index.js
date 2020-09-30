1ca7acad55e68e10eaaa1b31815c1ea07bec8c0e
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-09-30 01:52:59 -0300

    Added PROFILE functionality to the project

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -19,7 +19,7 @@ function Logon() {
         try {
             const response = await api.post('/sessions/', { id });
             
-            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongID', id);
             localStorage.setItem('ongName', response.data.name);
 
             history.push('/profile/');
